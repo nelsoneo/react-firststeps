44fa94900ae3510074a019dd7b87bf14c39d2217
nelsoneo <nelsoneo@gmail.com>
2021-09-30 22:17:49 -0300

    agregando css e bundler bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>My first app with React</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </>
   )
 }
 
